9b207c5b18b83ca1c7aa1107d364e0cd2594cf7e
suen_jr <frank.99030@gmail.com>
2023-12-18 22:03:20 +0800

    add Clerk Login Frame with only signIn work

---
+++ wp_fp_gambling/src/middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+    publicRoutes: ['/','/signIn','/signUp'],
+});
+
+export const config = {
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
