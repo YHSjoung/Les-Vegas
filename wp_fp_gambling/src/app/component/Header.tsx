9e3ce0994dc9386a7e1bddf461a79358389c792a
suen_jr <frank.99030@gmail.com>
2023-12-18 22:22:44 +0800

    Clerk login frame with accountProfile and logout

--- wp_fp_gambling/src/app/component/Header.tsx
+++ wp_fp_gambling/src/app/component/Header.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
+import { auth, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
-  return (
-    <>
-      <nav className="blue py-4 px-4 flex items-center justify-between">
-        <Link href="/">Gambling</Link>
-        <Link href="/signIn">SignIn</Link>
-        <Link href="/signOut">SignOut</Link>
-      </nav>
-    </>
-  );
+    const {userId} = auth();
+    console.log(userId);
+
+    return(
+        <>
+            <nav className="blue py-4 px-4 flex items-center justify-between">
+                <Link href='/'>
+                    Gambling
+                </Link>
+                {!userId && (
+                <Link href='/signIn'>
+                    SignIn
+                </Link>
+                )}
+                <UserButton afterSignOutUrl="/"/>
+            </nav>
+        </>
+    )
 };
 
-export default Header;
+export default Header; 
