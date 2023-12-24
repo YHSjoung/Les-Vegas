869d07b7b1810eacbb374873ccd97238c471f3bd
viola50420 <92725965+viola50420@users.noreply.github.com>
2023-12-25 04:58:44 +0800

    差page間routs

--- wp_fp_gambling/src/app/component/Header.tsx
+++ wp_fp_gambling/src/app/component/Header.tsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
 import { auth, UserButton } from "@clerk/nextjs";
+import Head from "next/head";
 
 const Header = () => {
-    const {userId} = auth();
-    console.log(userId);
+  const { userId } = auth();
 
-    return(
-        <>
-            <nav className="blue py-4 px-4 flex items-center justify-between">
-                <Link href='/'>
-                    Gambling
-                </Link>
-                {!userId && (
-                <Link href='/signIn'>
-                    SignIn
-                </Link>
-                )}
-                <UserButton afterSignOutUrl="/"/>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          integrity="sha512-..."
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <nav className="py-4 px-4 flex items-center justify-between">
+        <Link href="/" className="text-black text-xl font-bold">Gambling</Link>
+        {!userId && <Link href="/signIn" className="text-black text-xl font-bold">SignIn</Link>}
+        <UserButton afterSignOutUrl="/" />
+      </nav>
+    </>
+  );
 };
 
-export default Header; 
+export default Header;
--- wp_fp_gambling/src/app/layout.tsx
+++ wp_fp_gambling/src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
