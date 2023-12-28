3a8c115dca690c845c8613f70dc0e72736b74f38
suen_jr <frank.99030@gmail.com>
2023-12-28 15:18:07 +0800

    contract display with type

--- wp_fp_gambling/src/app/page.tsx
+++ wp_fp_gambling/src/app/page.tsx
@@ -7,7 +7,7 @@ import Contract from "./component/Contract";
 import { Suspense } from "react";
 import Loading from "./loading";
 import { db } from "@/db";
-import { eq, desc, isNull, sql } from "drizzle-orm";
+import { eq, desc, isNull, sql, and } from "drizzle-orm";
 import Link from "next/link";
 import { betsTable, contractTable } from "@/db/schema";
 
@@ -26,7 +26,7 @@ export default async function Home() {
       .where(eq(betsTable.userId, userId!)),
   );
 
-  const contracts = await db
+  const weatherContracts = await db
     .with(betSubquery)
     .select({
       contractId: contractTable.id,
@@ -42,23 +42,70 @@ export default async function Home() {
       dollar: betSubquery.dollar,
     })
     .from(contractTable)
-    .where(eq(contractTable.open, true))
+    .where(and(eq(contractTable.open, true), eq(contractTable.type, "weather")))
     .leftJoin(betSubquery, eq(contractTable.id, betSubquery.contractId))
-    // .groupBy(contractTable.type)
     .execute();
-  console.log(contracts);
+
+  const MarketingContracts = await db
+    .with(betSubquery)
+    .select({
+      contractId: contractTable.id,
+      title: contractTable.title,
+      description: contractTable.description,
+      optionA: contractTable.optionA,
+      optionB: contractTable.optionB,
+      optionC: contractTable.optionC,
+      totalDollar: contractTable.totalDollar,
+      attendees: contractTable.attendees,
+      blockDate: contractTable.blockDate,
+      option: betSubquery.option,
+      dollar: betSubquery.dollar,
+    })
+    .from(contractTable)
+    .where(and(eq(contractTable.open, true), eq(contractTable.type, "marketing")))
+    .leftJoin(betSubquery, eq(contractTable.id, betSubquery.contractId))
+    .execute();
+  
+  const sportContracts = await db
+    .with(betSubquery)
+    .select({
+      contractId: contractTable.id,
+      title: contractTable.title,
+      description: contractTable.description,
+      optionA: contractTable.optionA,
+      optionB: contractTable.optionB,
+      optionC: contractTable.optionC,
+      totalDollar: contractTable.totalDollar,
+      attendees: contractTable.attendees,
+      blockDate: contractTable.blockDate,
+      option: betSubquery.option,
+      dollar: betSubquery.dollar,
+    })
+    .from(contractTable)
+    .where(and(eq(contractTable.open, true), eq(contractTable.type, "sport")))
+    .leftJoin(betSubquery, eq(contractTable.id, betSubquery.contractId))
+    .execute();  
+
+  console.log(weatherContracts);
   return (
     <>
       <Header userId={userId!} />
       <Main />
       <Suspense fallback={<Loading />} />
-      {contracts.length === 0 ? (
+      {weatherContracts.length === 0 ? (
         <div className="flex justify-center">
-          <p className="text-4xl my-4 font-bold text-gray-400">Threre is not any contract</p>
+          <p className="text-4xl my-4 font-bold text-gray-400">Threre is not any weather contract</p>
         </div>
       ) : (
+        <>
+        <div className="flex justify-begin ml-10">
+          <p className="text-8xl mt-4 font-bold text-blue-400">Weather</p>
+        </div>
+        <hr className="border-t border-4 text-blue-400 mt-2 mx-4 rounded-md" />
+        <hr className="border-t border-4 text-blue-400 mx-4" />
+        <hr className="border-t border-4 text-blue-400 mx-4 rounded-xl" />
         <div className="grid grid-cols-2 gap-4 m-4">
-          {contracts.map((contract) => {
+          {weatherContracts.map((contract) => {
             console.log(contract);
             return (
               <Contract
@@ -77,6 +124,75 @@ export default async function Home() {
             );
           })}
         </div>
+        </>
+      )}
+      {MarketingContracts.length === 0 ? (
+        <div className="flex justify-center">
+          <p className="text-4xl my-4 font-bold text-gray-400">Threre is not any marketing contract</p>
+        </div>
+      ) : (
+      <>
+      <div className="flex justify-begin ml-10">
+        <p className="text-8xl mt-4 font-bold text-red-400">Marketing</p>
+      </div>
+      <hr className="border-t border-4 text-red-400 mt-2 mx-4 rounded-md" />
+      <hr className="border-t border-4 text-red-400 mx-4" />
+      <hr className="border-t border-4 text-red-400 mx-4 rounded-xl" />
+        <div className="grid grid-cols-2 gap-4 m-4">
+          {MarketingContracts.map((contract) => {
+            console.log(contract);
+            return (
+              <Contract
+                contractId={contract.contractId}
+                title={contract.title}
+                description={contract.description}
+                totalDollar={contract.totalDollar!}
+                optionA={contract.optionA!}
+                optionB={contract.optionB!}
+                optionC={contract.optionC!}
+                attendees={contract.attendees!}
+                blockDate={contract.blockDate!}
+                option={contract.option}
+                dollar={contract.dollar}
+              />
+            );
+          })}
+        </div>
+        </>
+      )}
+      {sportContracts.length === 0 ? (
+        <div className="flex justify-center">
+          <p className="text-4xl my-4 font-bold text-gray-400">Threre is not any sport contract</p>
+        </div>
+      ) : (
+      <>
+      <div className="flex justify-begin ml-10">
+        <p className="text-8xl mt-4 font-bold text-green-400">NBA</p>
+      </div>
+        <hr className="border-t border-4 text-green-400 mt-2 mx-4 rounded-md" />
+        <hr className="border-t border-4 text-green-400 mx-4" />
+        <hr className="border-t border-4 text-green-400 mx-4 rounded-xl" />
+          <div className="grid grid-cols-2 gap-4 m-4">
+            {sportContracts.map((contract) => {
+              console.log(contract);
+              return (
+                <Contract
+                  contractId={contract.contractId}
+                  title={contract.title}
+                  description={contract.description}
+                  totalDollar={contract.totalDollar!}
+                  optionA={contract.optionA!}
+                  optionB={contract.optionB!}
+                  optionC={contract.optionC!}
+                  attendees={contract.attendees!}
+                  blockDate={contract.blockDate!}
+                  option={contract.option}
+                  dollar={contract.dollar}
+                />
+              );
+            })}
+          </div>
+        </>
       )}
       
       <Footer />
