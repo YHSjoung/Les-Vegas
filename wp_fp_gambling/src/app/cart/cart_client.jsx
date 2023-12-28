bc85ad8e3f484cbf508aaad62a345de0863e9ec2
nopupu <apple912162@gmail.com>
2023-12-28 18:32:32 +0800

    bad cart

---
+++ wp_fp_gambling/src/app/cart/cart_client.jsx
@@ -0,0 +1,17 @@
+"use client";
+export default function EmptyCart() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 py-5 bg-light text-center">
+          <h4 className="p-3 display-5">Your Cart is Empty</h4>
+          <Link href="/" className="btn  btn-outline-dark mx-4">
+            <i className="fa fa-arrow-left"></i> Continue Shopping
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
