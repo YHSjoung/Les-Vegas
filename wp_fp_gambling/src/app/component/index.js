8a557c3633a9e80e9d04dbfead6783d2a4d0ca6f
suen_jr <frank.99030@gmail.com>
2023-12-30 15:40:34 +0800

    remove uselsee file

--- wp_fp_gambling/src/app/component/index.js
+++ wp_fp_gambling/src/app/component/index.js
@@ -1,4 +1,3 @@
 export { default as Navbar } from "./Navbar";
 export { default as Product } from "./Contract";
 export { default as Footer } from "./Footer";
-export { default as WrapperNav } from "./WrapperNav";
