379c9c42f9c38c5f1642aff94ba6173c688f515b
suen_jr <frank.99030@gmail.com>
2024-01-01 03:51:32 +0800

    api

--- wp_fp_gambling/src/middleware.ts
+++ wp_fp_gambling/src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/signIn", "/signUp","/api/cron"],
+  publicRoutes: ["/", "/signIn", "/signUp","/api/crons"],
   apiRoutes: ["/api(/*)?"],
 });
 
